ae2f220163b68ed9ae4465ec6adf0b7db2c27bb8
YanaSokol <90035722+YanaSokol@users.noreply.github.com>
2021-10-09 14:08:13 +0300

    Update AnimalsApp.java

--- AnimalsApp.java
+++ AnimalsApp.java
@@ -1,13 +1,13 @@
-package com.company;
-
 public class AnimalsApp {
 
     public static void main(String[] args) {
+        System.out.println("Кошка");
         Animal cat = new Cat(200, 2);
+        cat.run();
+        cat.swim();
+        System.out.println("Собака");
         Animal dog = new Dog(500, 3);
-        cat.run(300);
-        cat.swim(1);
-        dog.run(500);
-        dog.swim(3);
+        dog.run();
+        dog.swim();
     }
 }
